c9d06196e5e4be999c623d0b3ff826bfc089bba7
Serhii Davydov <serhii.davydov@gmail.com>
2023-03-09 15:31:21 +0200

    Implement 5-0-1

--- 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
+++ 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
@@ -1,12 +1,17 @@
 package com.bobocode.fp;
 
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 /**
- * An util class that provides a factory method for creating an instance of a {@link FunctionMap} filled with a list
+ * An util class that provides a factory method for creating an instance of a {@link FunctionMap}
+ * filled with a list
  * of functions.
  * <p>
  * TODO: implement a method and verify it by running {@link FunctionsTest}
  * <p>
- * TODO: if you find this exercise valuable and you want to get more like it, <a href="https://www.patreon.com/bobocode"> 
+ * TODO: if you find this exercise valuable and you want to get more like it,
+ * <a href="https://www.patreon.com/bobocode">
  *     please support us on Patreon</a>
  *
  * @author Taras Boychuk
@@ -29,6 +34,24 @@ public class Functions {
         FunctionMap<Integer, Integer> intFunctionMap = new FunctionMap<>();
 
         // todo: according to the javadoc add functions using lambda expression
+        UnaryOperator<Integer> abs = integer -> {
+            return integer >= 0 ? integer : integer * -1;
+        };
+        intFunctionMap.addFunction("abs", abs);
+
+        Function<Integer, Integer> sgn = integer -> {
+            return integer == 0 ? 0 : integer > 0 ? 1 : -1;
+        };
+        intFunctionMap.addFunction("sgn", sgn);
+
+        UnaryOperator<Integer> increment = integer -> integer + 1;
+        intFunctionMap.addFunction("increment", increment);
+
+        UnaryOperator<Integer> decrement = integer -> integer - 1;
+        intFunctionMap.addFunction("decrement", decrement);
+
+        UnaryOperator<Integer> square = integer -> integer * integer;
+        intFunctionMap.addFunction("square", square);
 
         return intFunctionMap;
     }
